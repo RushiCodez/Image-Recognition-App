1e7f6053f39fa3fbe1655b57958437c99cb398f7
RushiCodez <94935989+RushiCodez@users.noreply.github.com>
2022-03-20 11:52:38 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+webcam.set({
+width: 350px;
+height:300px;
+image_format:'png',
+png_quality:90;
+
+});
+
+camera= document.getElementById("camera");
+webcam.attatch('#camera');
+
+function take_snapshot(){
+webcam.snap(function(data_uri){  
+document.getElementById("result").innerHTML = '<img id= "captured_image" src= "'+ data_uri +'"/>';
+
+});
+}
+
+console.log("ml5 version", ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/swCMDa9SF/model.json", modelLoaded); 
+
+function modelLoaded(){
+console.log("Model 1oaded!")
+}
+
+function check(){
+img= document.getElementById("captured_image");
+classifier.classify(img, gotResult);
+}
+
+function gotResult(error,results){
+if error{
+console.error(error)
+
+}
+else {
+console.log(results);
+document.getElementById("result_object_name").innerHTML = results[0].label;
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+
+
+}
+
+}
+
+
